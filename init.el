3e9b645aea2941abf700c0c5e2e5037be627b988
Denis Dowling <denis.dowling@raedyne.systems>
2021-02-08 21:49:09 +1100

    Added support for editting YAML and Arduino files

--- init.el
+++ init.el
@@ -19,3 +19,14 @@ indent-tabs-mode nil)
 
 ;; Remove trailing whitespace on save
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Look for extra packages in this directory
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Handle the YAML configuration files that Ubuntu seems to love
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+
+;; Some Arduino editting support
+(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
